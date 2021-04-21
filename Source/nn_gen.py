30ec0caf96add5801e301ddfbc0fd0b5899aad30
shivi47 <shivi47@users.noreply.github.com>
2021-04-21 15:48:17 -0400

    cleaned up

--- Source/nn_gen.py
+++ Source/nn_gen.py
@@ -2,27 +2,7 @@ from torch import nn
 import torch
 
 class RNN(nn.Module):
-    """
-    def __init__(self, n_dim, seq_len, n_hidden, n_layers=1):
-        super(RNN, self).__init__()
 
-        self.rnn = nn.LSTM(n_dim, n_hidden, n_layers, batch_first=True)
-        self.fc = nn.Linear(n_hidden * seq_len, n_dim)
-
-    def forward(self, x, times=1):
-        x, h = self.rnn(x)
-        x = self.fc(x)
-        outs = []
-        outs.append(x)
-        for i in range(times-1):
-            x, h = self.rnn(x, h)
-            x = self.fc(x)
-            outs.append(x)
-        if times > 1:
-            return outs
-        b, s, h = x.shape
-        return x
-    """
     def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
         super(RNN, self).__init__()
         # Hidden dimensions

44669079912857a08146bbc9016c06be4823c83b
Noura <nourabayat@outlook.com>
2021-04-22 15:39:00 -0400

    added backprop function

--- Source/nn_gen.py
+++ Source/nn_gen.py
@@ -7,39 +7,51 @@ class RNN(nn.Module):
         super(RNN, self).__init__()
         # Hidden dimensions
         self.hidden_dim = hidden_dim
-
         # Number of hidden layers
         self.layer_dim = layer_dim
-
         # Building your LSTM
         # batch_first=True causes input/output tensors to be of shape
         # (batch_dim, seq_dim, feature_dim)
         self.lstm = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first=True)
-
         # Readout layer
         self.fc = nn.Linear(hidden_dim, output_dim)
-
+        
     def forward(self, x):
         # Initialize hidden state with zeros
         h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
-
         # Initialize cell state
         c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
-
         # 28 time steps
         # We need to detach as we are doing truncated backpropagation through time (BPTT)
         # If we don't, we'll backprop all the way to the start even after going through another batch
         out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
-
         # Index hidden state of last time step
         # out.size() --> 100, 28, 100
         # out[:, -1, :] --> 100, 100 --> just want last time step hidden states!
         out = self.fc(out[:, -1, :])
         # out.size() --> 100, 10
         return out
+    
+    # Reset function for the training weights
+    # Use if the same network is trained multiple times
+    def reset(self):
+        self.fc1.reset_parameters()
+        self.fc2.reset_parameters()
+    
+    def backprop(self, train_in, train_out, loss, optimizer):
+        self.train()
+        inputs = torch.from_numpy(train_in)
+        targets = torch.from_numpy(train_out)
+        outputs = self.forward(inputs)
 
-
-    def get_accuracy(inputs, targets):
+        acc = get_accuracy(outputs, targets)
+        obj_val = loss(outputs, targets)
+        optimizer.zero_grad()
+        obj_val.backward()
+        optimizer.step()
+        return obj_val.item(), acc
+    
+    def get_accuracy(self, inputs, targets):
         """
         Given the output of the nn and the target,
         compute the accuracy which is the percent of correct predictions over all
@@ -59,12 +71,13 @@ class RNN(nn.Module):
 
         accuracy = correct_pred / total_pred
         return accuracy
-    # Test function. Avoids calculation of gradients.
-    def test(self, test_in, test_out, loss):
+    
+    def test(self, x_test, x_train,loss):
         self.eval()
         with torch.no_grad():
-            inputs= test_in
-            targets= test_out
-            outputs= self(inputs)
-            cross_val= loss(self.forward(inputs), targets)
-        return cross_val.item()
+            inputs= torch.from_numpy(x_test)
+            targets= torch.from_numpy(y_test)
+            outputs= self.forward(inputs)
+            acc = get_accuracy(outputs, targets)
+            cross_val = loss(outputs, targets)
+        return cross_val.item(), acc

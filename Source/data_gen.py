a9f8b14008e195e2668be4f9a142b0a0ee0d210d
fahim <fsyusufz@uwaterloo.ca>
2021-04-22 08:20:12 -0400

    2 nodes --> 4 nodes

--- Source/data_gen.py
+++ Source/data_gen.py
@@ -20,32 +20,38 @@ class Data():
         logical_err_train = np.array(logical_err_train, dtype=np.int)
         #print(logical_err_train, '\n')
         #print(err_synd_train.shape, err_synd_train.dtype, logical_err_train.shape, logical_err_train.dtype)
-        print(logical_err_train)
+
         self.err_synd_train = err_synd_train
         self.logical_err_train = logical_err_train
-        print(lg_train)
         lg_test = data_test[:49, 0:2]
-        lg_test.tolist()
         N = len(np.array(lg_test)[:, 0])
-        print(N)
+
+        def logic(L):
+
+            M=np.zeros(4, dtype=int)
+
+            if (L[0]==0 and L[1]==0):
+                M[0]=1
+            if (L[0]==0 and L[1]==1):
+                M[1]=1
+            if (L[0]==1 and L[1]==0):
+                M[2]=1
+            if (L[0]==1 and L[1]==1):
+                M[3]=1
+
+            return np.array(M)
+
+
+        new_lg_test = np.zeros((N, 4), dtype=int)
         for i in range(N):
-            print(lg_test)
-            if lg_test[i, 0] and lg_test[i, 1] == 0:
-                lg_test[i, :] = [1,0,0,0]
-            print(lg_test)
-            if lg_test[i, 0] == 1 and lg_test[i, 1] == 0:
-                lg_test[i, :] = [0,1,0,0]
-            if lg_test[i, 0] == 0 and lg_test[i, 1] == 1:
-                lg_test[i, :] = [0,0,1,0]
-            if lg_test[i, 0] and lg_test[i, 1] == 1:
-                lg_test[i, :] = [0,0,0,1]
+            new_lg_test[i] = (logic(lg_test[i,:]))
+        print(new_lg_test)
 
 
         err_synd_test = data_test[:49, -8:]
         logical_err_test = np.array(lg_test)
         err_synd_test = np.array(err_synd_test, dtype=np.int)
         logical_err_test = np.array(logical_err_test, dtype=np.int)
-        print(err_synd_test)
         #print(logical_err_train, '\n')
         #print(err_synd_train.shape, err_synd_train.dtype, logical_err_train.shape, logical_err_train.dtype)
 
@@ -61,7 +67,7 @@ if __name__ == '__main__':
     x_test, y_test = for_training.err_synd_test, for_training.logical_err_test
 
     #print('\nsyndrome measurements for training: \n', x)
-    print('\n',x.shape, x.dtype)
+    print('\n',x_train.shape, x_train.dtype)
 
     #print('\nlogical gates in array of 0s and 1s: \n', y)
-    print('\n',y.shape, y.dtype,'\n')
+    print('\n',y_train.shape, y_train.dtype,'\n')
